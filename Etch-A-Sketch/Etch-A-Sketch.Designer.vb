b02a0c8652451d84b8dcb9aa4a1ac28d5006efd3
bbre1666 <badenbrenner@isu.edu>
2023-04-11 21:15:42 -0600

    Add project files.

---
+++ Etch-A-Sketch/Etch-A-Sketch.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class EtchASketch
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SelectColorButton = New System.Windows.Forms.Button()
+        Me.WaveformsButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.PictureBox = New System.Windows.Forms.PictureBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ColorDialog = New System.Windows.Forms.ColorDialog()
+        Me.ContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SelectColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DrawWaveformsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GradiculesButton = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SelectColorButton
+        '
+        Me.SelectColorButton.Location = New System.Drawing.Point(12, 409)
+        Me.SelectColorButton.Name = "SelectColorButton"
+        Me.SelectColorButton.Size = New System.Drawing.Size(143, 29)
+        Me.SelectColorButton.TabIndex = 0
+        Me.SelectColorButton.Text = "Select Color"
+        Me.ToolTip1.SetToolTip(Me.SelectColorButton, "Color Slection")
+        Me.SelectColorButton.UseVisualStyleBackColor = True
+        '
+        'WaveformsButton
+        '
+        Me.WaveformsButton.Location = New System.Drawing.Point(174, 409)
+        Me.WaveformsButton.Name = "WaveformsButton"
+        Me.WaveformsButton.Size = New System.Drawing.Size(142, 29)
+        Me.WaveformsButton.TabIndex = 1
+        Me.WaveformsButton.Text = "Draw Waveforms"
+        Me.ToolTip1.SetToolTip(Me.WaveformsButton, "Plots Waveforms")
+        Me.WaveformsButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(334, 409)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(137, 29)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "Clear"
+        Me.ToolTip1.SetToolTip(Me.ClearButton, "Clears Deawings ")
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'PictureBox
+        '
+        Me.PictureBox.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox.Name = "PictureBox"
+        Me.PictureBox.Size = New System.Drawing.Size(776, 391)
+        Me.PictureBox.TabIndex = 3
+        Me.PictureBox.TabStop = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(653, 409)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(135, 29)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "Exit"
+        Me.ToolTip1.SetToolTip(Me.ExitButton, "Closes Form")
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ContextMenuStrip
+        '
+        Me.ContextMenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectColorToolStripMenuItem, Me.DrawWaveformsToolStripMenuItem, Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip.Name = "ContextMenuStrip"
+        Me.ContextMenuStrip.Size = New System.Drawing.Size(192, 100)
+        '
+        'SelectColorToolStripMenuItem
+        '
+        Me.SelectColorToolStripMenuItem.Name = "SelectColorToolStripMenuItem"
+        Me.SelectColorToolStripMenuItem.Size = New System.Drawing.Size(191, 24)
+        Me.SelectColorToolStripMenuItem.Text = "Select Color"
+        '
+        'DrawWaveformsToolStripMenuItem
+        '
+        Me.DrawWaveformsToolStripMenuItem.Name = "DrawWaveformsToolStripMenuItem"
+        Me.DrawWaveformsToolStripMenuItem.Size = New System.Drawing.Size(191, 24)
+        Me.DrawWaveformsToolStripMenuItem.Text = "Draw Waveforms"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(191, 24)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(191, 24)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'GradiculesButton
+        '
+        Me.GradiculesButton.Location = New System.Drawing.Point(491, 409)
+        Me.GradiculesButton.Name = "GradiculesButton"
+        Me.GradiculesButton.Size = New System.Drawing.Size(137, 29)
+        Me.GradiculesButton.TabIndex = 5
+        Me.GradiculesButton.Text = "Gradicules"
+        Me.ToolTip1.SetToolTip(Me.GradiculesButton, "Plots Gradicules")
+        Me.GradiculesButton.UseVisualStyleBackColor = True
+        '
+        'EtchASketch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GradiculesButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.PictureBox)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.WaveformsButton)
+        Me.Controls.Add(Me.SelectColorButton)
+        Me.Name = "EtchASketch"
+        Me.Text = "Etch-A-Sketch"
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SelectColorButton As Button
+    Friend WithEvents WaveformsButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents PictureBox As PictureBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ColorDialog As ColorDialog
+    Friend WithEvents ContextMenuStrip As ContextMenuStrip
+    Friend WithEvents SelectColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DrawWaveformsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GradiculesButton As Button
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ Etch-A-Sketch/Etch-A-Sketch.vb
@@ -0,0 +1,193 @@
+﻿Option Strict On
+Option Explicit On
+Imports System.Runtime.Intrinsics.X86
+
+'Baden Brenner
+'RCET0265
+'Spring 2023
+'Etch-A-Sketch
+'
+
+
+Public Class EtchASketch
+    'Globals
+    Dim MainPen As New Pen(Color.Black)
+    Dim GradiculeState As Boolean = False
+    Dim WaveFormState As Boolean = False
+    Dim scopelines As Bitmap
+    Dim Waves As Bitmap
+    Dim Blank As Bitmap
+    Dim Wavesscopelines As Bitmap
+    Dim currentColor As Color
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        'Clear button
+        Clear() 'Runs Clear Sub
+    End Sub
+    Sub Clear()
+        PictureBox.BackColor = Color.White 'Set backround of pictureBox
+        PictureBox.Refresh() 'Refresh pictureBox 
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        currentColor = Color.Black 'setting pen color
+    End Sub
+
+
+    Private Sub PictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox.MouseMove
+        Static lastX%, lastY%
+        Select Case e.Button.ToString 'Trackes mouse movement and button clicks 
+            Case "Left"
+                DrawLineSegment(lastX, lastY, e.X, e.Y)
+            Case "Middle"
+                UpdateColor()
+            Case "Right"
+                ContextMenuStrip.Show()
+        End Select
+        Me.Text = $"({e.X},{e.Y}) button: {e.Button.ToString} Color: {Me.currentColor.ToString}"
+        lastX = e.X
+        lastY = e.Y
+    End Sub
+    Sub DrawLineSegment(x1%, y1%, x2%, y2%) 'drawing the line
+        Dim g As Graphics = PictureBox.CreateGraphics
+        Dim myPen = New Pen(Me.currentColor)
+
+        g.DrawLine(myPen, x1, y1, x2, y2)
+
+        myPen.Dispose()
+        g.Dispose()
+    End Sub
+    Sub GraphWaveform() 'creating the bitmap fot the graphed waveforms 
+        Dim picBoxWidth As Integer = PictureBox.Width ' Define variables for the picture box dimensions
+        Dim picBoxHeight As Integer = PictureBox.Height
+
+
+        ' Create a bitmap to draw the sine wave on
+        Dim Waveforms As New Bitmap(picBoxWidth, picBoxHeight) 'setting the bitmap size 
+
+        ' Set up the graphics object
+        Dim g As Graphics = Graphics.FromImage(Waveforms)
+
+        ' Define variables for the sine wave
+        Dim amplitude As Double = picBoxHeight / 4 ' the amplitude of the wave
+        Dim frequency As Double = 2 * Math.PI / picBoxWidth ' the frequency of the wave
+
+        ' Draw the sine wave
+        For x As Integer = 0 To picBoxWidth - 1
+            Dim y As Integer = CInt(amplitude * Math.Sin(frequency * x))
+            Waveforms.SetPixel(x, picBoxHeight \ 2 - y, Color.Black)
+        Next
+        ' Draw the Cosine wave
+        For x As Integer = 0 To picBoxWidth - 1
+            Dim y As Integer = CInt(amplitude * Math.Cos(frequency * x))
+            Waveforms.SetPixel(x, picBoxHeight \ 2 - y, Color.Red)
+        Next
+        ' Draw the tan curve 
+        Try
+            For x As Integer = 0 To picBoxWidth - 1
+                Dim y As Integer = CInt(amplitude * Math.Tan(frequency * x))
+                Waveforms.SetPixel(x, picBoxHeight \ 2 - y, Color.Green)
+            Next
+        Catch
+        End Try
+        ' Draw the bitmap on the picture box
+        Waves = Waveforms
+    End Sub
+
+    Sub UpdateColor() 'sub for updating the color of the pen
+        ColorDialog.ShowDialog()
+        Me.currentColor = ColorDialog.Color
+    End Sub
+    Private Sub SelectColorButton_Click(sender As Object, e As EventArgs) Handles SelectColorButton.Click
+        UpdateColor() 'refferenceing the update color sub
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close() 'closes the form
+    End Sub
+    Sub scopegradicules() 'creating the bitmap with to plot the gradicules  
+        Dim picBoxWidth As Integer = PictureBox.Width ' Define variables for the picture box dimensions
+        Dim picBoxHeight As Integer = PictureBox.Height
+        Dim GridSize As Integer = CInt(77.5) 'setting the size of the grid
+
+        Dim Gradicules As New Bitmap(picBoxWidth, picBoxHeight) 'setting the bitmap size 
+
+        Dim G As Graphics = Graphics.FromImage(Gradicules)
+        ' Draw the horizontal lines
+        For y As Integer = 0 To picBoxHeight - 1 Step GridSize
+            G.DrawLine(Pens.Black, New Point(0, y), New Point(picBoxWidth, y))
+        Next
+
+        ' Draw the vertical lines
+        For x As Integer = 0 To picBoxWidth - 1 Step GridSize
+            G.DrawLine(Pens.Black, New Point(x, 0), New Point(x, picBoxHeight))
+        Next
+
+        ' Draw the bitmap on the picture box
+
+        scopelines = Gradicules
+    End Sub
+    Private Sub WaveformsButton_Click(sender As Object, e As EventArgs) Handles WaveformsButton.Click
+
+        If WaveFormState = False Then 'checking the wavefrom latch and the state of it 
+            WaveFormState = True
+        ElseIf WaveFormState = True Then
+            WaveFormState = False
+        End If
+
+        If WaveFormState = True Then ' displaying or clearing the waveform
+            GraphWaveform()
+            PictureBox.Image = Waves
+
+        ElseIf WaveFormState = False Then
+            Clear()
+            PictureBox.Image = Blank
+        End If
+        BothMaps() 'checking for both gradicules and waveforms
+
+    End Sub
+
+    Private Sub gradicules_Click(sender As Object, e As EventArgs) Handles GradiculesButton.Click
+
+        If GradiculeState = False Then  'checking the Gradicules latch and the state of it 
+            GradiculeState = True
+        ElseIf GradiculeState = True Then
+            GradiculeState = False
+        End If
+
+        If GradiculeState = True Then ' displaying or clearing the Gradicules
+            scopegradicules()
+            PictureBox.Image = scopelines
+
+        ElseIf GradiculeState = False Then
+            Clear()
+            PictureBox.Image = Blank
+        End If
+        BothMaps() 'checking for both gradicules and waveforms
+    End Sub
+    Private Sub BothMaps()
+        'this sub is checked when ether the waveform and Gradicule button is clicked to see if they need to be displayed together 
+
+        If GradiculeState And WaveFormState Then
+            Dim picBoxWidth As Integer = PictureBox.Width ' Define variables for the picture box dimensions
+            Dim picBoxHeight As Integer = PictureBox.Height
+
+            ' Create a new bitmap to hold the combined images adn setting the size 
+            Dim combinedBmp As New Bitmap(picBoxWidth, picBoxHeight)
+
+            Dim g As Graphics = Graphics.FromImage(combinedBmp)
+
+
+            g.DrawImage(Waves, New Point(0, 0))
+
+
+            g.DrawImage(scopelines, New Point(10, 10))
+
+
+            g.Dispose()
+
+            ' Save the combined image to a file or display it in a picture box
+            combinedBmp.Save("combined.bmp")
+
+            PictureBox.Image = combinedBmp 'displaying the combined bitmap
+        End If
+    End Sub
+End Class
